817390c6cb00c3c7a3aaa081362496d6a5f62e56
Aunshon <32583103+Aunshon@users.noreply.github.com>
2024-10-09 03:24:35 +0600

    Fix file structure error.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { useWatchParam } from './hooks/useWatchParam';
+export { useQueryNavigation } from './hooks/useQueryNavigation';
+export { QueryLink } from './components/QueryLink';
+export { Routes } from './components/Routes';
+export { Route } from './components/Route';
